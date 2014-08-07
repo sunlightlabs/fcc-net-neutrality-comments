324b7c9ecd45256166c3bc1cedf3d267684cac8b
Bob Lannon <blannon@gmail.com>
2014-08-07 15:21:44 -0400

    skipping first line of csv

--- scripts/process_all_json.sh
+++ scripts/process_all_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tail stats/reasonable_length_files.csv | cut -d , -f 1 | xargs -P0 -n1 -I {} python models/pos_tagger.py data/json/processed {}
+tail -n+2 stats/reasonable_length_files.csv | cut -d , -f 1 | xargs -P0 -n1 -I {} python models/pos_tagger.py data/json/processed {}
