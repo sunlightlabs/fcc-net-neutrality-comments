6265eec9741549cce74a2cea529b1a1df2cc1b46
Bob Lannon <blannon@gmail.com>
2014-08-07 23:55:48 -0400

    added processed dir and tokenizer for gposttl'd text

--- models/tokenizer.py
+++ models/tokenizer.py
@@ -1,3 +1,6 @@
+import re
+import sys
+
 from nltk import word_tokenize
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
@@ -28,4 +31,38 @@ class DefaultTokenizer(BaseTokenizer):
             tokens = [self.stemmer.stem(word.lower())
                       for word in words
                       if word not in self.stopwords and word.isalnum()]
+            return tokens
+
+
+class PretaggedTokenizer(BaseTokenizer):
+
+    def __init__(self, stopword_list=None, filter_tags=None):
+        self.regexp = re.compile('(.+?)\|([A-Z\.\,:\)\(\$)\"]+)\|(.+)')
+        if filter_tags:
+            self.filter_tags = filter_tags
+        else:
+            self.filter_tags = list()
+        super(PretaggedTokenizer, self).__init__(stopword_list)
+
+    def tokenize(self, tagged_text):
+        tokens = []
+        try:
+            tagged_words = tagged_text.split()
+            for tagged_word in tagged_words:
+                try:
+                    word, pos, lemma = self.regexp.findall(tagged_word)[0]
+                    if lemma == '<unknown>':
+                        lemma = word
+                except ValueError:
+                    sys.stderr.write('problem word: '+tagged_word+'\n')
+                    #raise
+                else:
+                    if (not word.isalnum()) or (word.lower() in self.stopwords) or (pos in self.filter_tags):
+                        continue
+                    else:
+                        tokens.append('{s}_{pos}'.format(s=lemma, pos=pos))
+        except Exception:
+            sys.stderr.write('problem text: '+tagged_text+'\n')
+            raise
+        else:
             return tokens
