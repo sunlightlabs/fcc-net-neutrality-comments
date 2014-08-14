8af8a62dc6f7d71a0b5b3bbf999fe53463aaa187
Bob Lannon <blannon@gmail.com>
2014-08-14 11:55:49 -0400

    updated import loc

--- scripts/transform_corpus.py
+++ scripts/transform_corpus.py
@@ -6,7 +6,7 @@
 import sys
 import os
 
-sys.path.append(os.path.join(os.getcwd(), os.path.pardir))
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.path.pardir))
 
 
 # In[2]:
