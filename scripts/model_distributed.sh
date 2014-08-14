c84ccff5cdb74e80324db90fa3aafce64aae04b4
Bob Lannon <blannon@gmail.com>
2014-08-14 11:44:52 -0400

    parameterizing core number

--- scripts/model_distributed.sh
+++ scripts/model_distributed.sh
@@ -5,18 +5,19 @@ export PYRO_SERIALIZER=pickle
 
 MODEL_TYPE=$1
 NUM_TOPICS=$2
+NUM_CORES=$3
 
 PID_FILE='/tmp/gensim_pids'
 
 python -m Pyro4.naming -n 0.0.0.0 &
 echo $! > $PID_FILE
 
-taskset -c 1 python -m gensim.models.${MODEL_TYPE}_worker &
-echo $! >> $PID_FILE
-taskset -c 2 python -m gensim.models.${MODEL_TYPE}_worker &
-echo $! >> $PID_FILE
-taskset -c 3 python -m gensim.models.${MODEL_TYPE}_worker &
-echo $! >> $PID_FILE
+for i in `seq 1 $NUM_CORES`
+do
+	taskset -c $i python -m gensim.models.${MODEL_TYPE}_worker &
+	echo $! >> $PID_FILE
+done
+
 taskset -c 0 python -m gensim.models.${MODEL_TYPE}_dispatcher &
 echo $! >> $PID_FILE
 
