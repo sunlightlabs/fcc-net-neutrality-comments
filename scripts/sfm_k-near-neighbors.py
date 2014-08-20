d3744239d9ee8466e37d92739ef29d1d919c674a
Bob Lannon <blannon@gmail.com>
2014-08-20 03:22:34 -0400

    quick script to get the upper right triangle of a full NxN document comparison from superfastmatch

---
+++ scripts/sfm_k-near-neighbors.py
@@ -0,0 +1,44 @@
+import csv
+import sys
+
+import superfastmatch
+from multiprocessing.dummy import Pool
+
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
+                    filename='log/sfm_k-near-neighbors.log', filemode='a',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__file__)
+
+sfm_url = 'http://{host}:{port}/'.format(host='54.87.63.230', port='8080')
+sfm_client = superfastmatch.client.Client(url=sfm_url)
+
+sfm_documents = superfastmatch.iterators.DocumentIterator(sfm_client, 'docid',
+                                                          doctype=1,
+                                                          fetch_text=True,
+                                                          chunksize=100)
+
+
+def get_neighbors(document):
+    res = sfm_client.search(document['text'])
+    neighbors = [(document['title'], r['title'], r['fragment_count'])
+                 for r in res['documents']['rows']
+                 if document['docid'] < r['docid']]
+    for n in neighbors:
+        yield n
+
+
+pool = Pool(sys.argv[1])
+
+#result = pool.map_async(get_neighbors, [sfm_documents.next() for i in xrange(30)])
+result = pool.map_async(get_neighbors, sfm_documents)
+
+pool.close()
+pool.join()
+
+
+with open('persistence/sfm_knn.csv', 'w') as fout:
+    cw = csv.writer(fout)
+    for neighborset in result.get():
+        cw.writerows(neighborset)
