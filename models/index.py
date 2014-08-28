d3349c68300308e755764b55d3beed62b9f51207
Bob Lannon <blannon@gmail.com>
2014-08-28 11:54:50 -0400

    shitty inverted index

---
+++ models/index.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+
+class InvertedIndex(object):
+    def __init__(self, lazy_corpus, dictionary):
+        self.word_dictionary = dictionary
+        self.invert_corpus(lazy_corpus)
+
+    def invert_corpus(self, lazy_corpus):
+        self.token2docs = defaultdict(list)
+        for docid, doc in enumerate(lazy_corpus):
+            for token, count in doc:
+                self.token2docs[token].append((doc,count))
