d230eb1f55e3df08dc6acac800bbc3dd1d9e6944
Bob Lannon <blannon@gmail.com>
2015-01-13 01:08:10 -0500

    take in everything less than 600kb

--- scripts/process_all_json.sh
+++ scripts/process_all_json.sh
@@ -2,6 +2,6 @@
 
 num_cores=$1
 
-find data/json/raw -type f -size -10k | xargs -P $num_cores -n1 -I {} python models/pos_tagger.py data/json/processed {}
+find data/json/raw -type f -size -600k | xargs -P $num_cores -n1 -I {} python models/pos_tagger.py data/json/processed {}
 
 find data/json/processed -type f | xargs -n1 -I {} basename {} .json | sort > persistence/processed_document_ids
